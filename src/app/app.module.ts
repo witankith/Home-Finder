112314afb5f2b4e45d427925cf1e0ee4e1356565
witankith <ankithn56@gmail.com>
2017-10-17 15:43:56 -0500

    Home Finder

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { CribListingComponent } from './crib-listing/crib-listing.component';
+import { CribCardComponent } from './crib-card/crib-card.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CribListingComponent,
+    CribCardComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/crib.ts
@@ -0,0 +1,2 @@
+export interface Crib {
+}
